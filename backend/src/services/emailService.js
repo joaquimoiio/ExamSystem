36890cbef623e65f8c6e7d27585d5451fc1cd089
joaquimoiio <joaquimpaes01@gmail.com>
2025-08-12 14:26:31 -0300

    tentnado rodar o back

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 class EmailService {
   constructor() {
-    this.transporter = nodemailer.createTransporter({
+    this.transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST || 'smtp.gmail.com',
       port: process.env.EMAIL_PORT || 587,
       secure: false, // true for 465, false for other ports
