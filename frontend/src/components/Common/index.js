aa7c8316f8805cfd8f335f8b3dcb55cc2c3efda1
joaquimoiio <joaquimpaes01@gmail.com>
2025-08-14 09:53:52 -0300

    frontend v10

---
+++ frontend/src/components/Common/index.js
@@ -0,0 +1,32 @@
+// frontend/src/components/Common/index.js
+
+// Componentes básicos
+export { default as Button } from './Button'
+export { default as Input } from './Input'
+export { default as Modal, ConfirmationModal } from './Modal'
+export { default as Loading } from './Loading'
+export { default as LoadingSpinner } from './LoadingSpinner'
+
+// Componentes de navegação
+export { default as Pagination } from './Pagination'
+
+// Componentes de estado
+export { default as EmptyState } from './EmptyState'
+
+// Componentes de feedback
+export { default as Toast, showOperationToast } from './Toast'
+
+// Componentes de layout
+export { default as Card } from './Card'
+export { default as Badge } from './Badge'
+
+// Componentes de formulário
+export { default as Select } from './Select'
+export { default as Textarea } from './Textarea'
+export { default as Checkbox } from './Checkbox'
+export { default as Radio } from './Radio'
+
+// Outros componentes utilitários que podem existir
+// export { default as Dropdown } from './Dropdown'
+// export { default as Tooltip } from './Tooltip'
+// export { default as Alert } from './Alert'
